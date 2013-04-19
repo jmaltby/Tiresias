36c3e8b6fa4f0a65d6fbd98a33b6b2d47e47f2c0
jmaltby <johnnymaltby@gmail.com>
2013-04-19 10:48:06 -0500

    The complete directory of sound files has been added to the SeaBee repo

---
+++ WAV files/audio.py
@@ -0,0 +1,15 @@
+import pyglet
+
+startup = pyglet.media.load('startup.wav', streaming=False)
+shutdown = pyglet.media.load('shutdown.wav', streaming=False)
+north = pyglet.media.load('north.wav', streaming=False)
+south = pyglet.media.load('south.wav', streaming=False)
+east = pyglet.media.load('east.wav', streaming=False)
+west = pyglet.media.load('west.wav', streaming=False)
+fx_bell = pyglet.media.load('fx_bell.wav', streaming=False)
+fx_plop = pyglet.media.load('fx_plop.wav', streaming=False)
+fx_up = pyglet.media.load('fx_up.wav', streaming=False)
+fx_down = pyglet.media.load('fx_down.wav', streaming=False)
+
+#fx_bell.play()
+#fx_bell.pause()
